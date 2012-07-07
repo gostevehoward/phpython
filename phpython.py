26ac21cb6cd923a41c3c76ffbc271b60d53e76de
Steve Howard <dev@gostevehoward.com>
2012-07-07 14:08:28 -0700

    simplify Node usage, get rid of visitor, consolidate PHP parsing

--- phpython.py
+++ phpython.py
@@ -20,86 +20,77 @@ NS_ATTRIBUTE = '{http://nikic.github.com/PHPParser/XML/attribute}'
 NS_SCALAR = '{http://nikic.github.com/PHPParser/XML/scalar}'
 
 class Node(object):
-    def __init__(self, node_type, subnodes):
+    def __init__(self, node_type, subnode_map):
         self.type = node_type
-        self.subnodes = subnodes
+        self._subnode_map = subnode_map
 
-    def accept(self, visitor):
-        return visitor.visit_node(self)
+    def __getitem__(self, name):
+        return self._subnode_map[name]
 
-class Array(object):
-    def __init__(self, elements):
-        self.elements = elements
-
-    def accept(self, visitor):
-        return visitor.visit_array(self.elements)
-
-class Scalar(object):
-    def __init__(self, value):
-        self.value = value
-
-    def accept(self, visitor):
-        return visitor.visit_scalar(self.value)
+    def subnodes(self):
+        return self._subnode_map.iteritems()
 
 def remove_namespace(tag):
     return tag.rpartition('}')[2]
 
-def read_thing(element):
-    if element.tag.startswith(NS_NODE):
-        return read_node(element)
-    elif element.tag.startswith(NS_SCALAR):
-        return read_scalar(element)
-    else:
-        raise ValueError('Do not know how to read %r' % element.tag)
-
-def read_node(element):
-    assert element.tag.startswith(NS_NODE)
-    subnodes = {}
-    for child in element:
-        if child.tag.startswith(NS_SUBNODE):
-            assert len(child) == 1, child
-            tag  = remove_namespace(child.tag)
-            body = read_thing(child[0])
-            subnodes[tag] = body
-        elif child.tag.startswith(NS_ATTRIBUTE):
-            pass # ignore "attributes"
+class XmlPhpParseTreeReader(object):
+    def _read_element(self, element):
+        if element.tag.startswith(NS_NODE):
+            return self._read_node(element)
+        elif element.tag.startswith(NS_SCALAR):
+            return self._read_scalar(element)
         else:
-            raise ValueError('Do not know how to read %r within node' % child.tag)
-    return Node(remove_namespace(element.tag), subnodes)
+            raise ValueError('Do not know how to read %r' % element.tag)
 
-def read_scalar(element):
-    assert element.tag.startswith(NS_SCALAR)
-    tag = remove_namespace(element.tag)
-    if tag == 'array':
-        return Array([read_thing(child) for child in element])
-    elif tag == 'string':
-        return Scalar(element.text)
-    elif tag == 'true':
-        return Scalar(True)
-    elif tag == 'false':
-        return Scalar(False)
-    elif tag == 'int':
-        return Scalar(int(element.text))
-    elif tag == 'null':
-        return Scalar(None)
-    else:
-        raise ValueError('Unknown scalar type %r' % tag)
+    def _read_node(self, element):
+        assert element.tag.startswith(NS_NODE)
+        subnode_map = {}
+        for child in element:
+            if child.tag.startswith(NS_SUBNODE):
+                assert len(child) == 1, child
+                tag  = remove_namespace(child.tag)
+                body = self._read_element(child[0])
+                subnode_map[tag] = body
+            elif child.tag.startswith(NS_ATTRIBUTE):
+                pass # ignore "attributes"
+            else:
+                raise ValueError('Do not know how to read %r within node' % child.tag)
+        return Node(remove_namespace(element.tag), subnode_map)
 
-def parse_php(stream):
-    process = subprocess.Popen(
-        ['php', PARSE_PHP_SCRIPT],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-    )
-    xml, stderr = process.communicate(stream.read())
-    xml_root = ElementTree.fromstring(xml)
+    def _read_scalar(self, element):
+        assert element.tag.startswith(NS_SCALAR)
+        tag = remove_namespace(element.tag)
+        if tag == 'array':
+            return [self._read_element(child) for child in element]
+        elif tag == 'string':
+            return element.text
+        elif tag == 'true':
+            return True
+        elif tag == 'false':
+            return False
+        elif tag == 'int':
+            return int(element.text)
+        elif tag == 'null':
+            return None
+        else:
+            raise ValueError('Unknown scalar type %r' % tag)
 
-    assert xml_root.tag == 'AST'
-    assert len(xml_root) == 1
-    statements = xml_root[0]
-    assert statements.tag == NS_SCALAR + 'array'
+    def parse_tree(self, xml_root):
+        assert xml_root.tag == 'AST'
+        assert len(xml_root) == 1
+        statements = xml_root[0]
+        assert statements.tag == NS_SCALAR + 'array'
+        return self._read_element(statements)
 
-    return read_thing(statements)
+    def parse_php(self, stream):
+        process = subprocess.Popen(
+            ['php', PARSE_PHP_SCRIPT],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        )
+        xml, stderr = process.communicate(stream.read())
+        xml_root = ElementTree.fromstring(xml)
+        return self.parse_tree(xml_root)
 
 class PrettyFormatter(object):
     def __init__(self):
@@ -108,7 +99,7 @@ class PrettyFormatter(object):
 
     def pretty_format(self, thing):
         self._accumulator = []
-        thing.accept(self)
+        self._do_format(thing)
         return ''.join(self._accumulator)
 
     @contextlib.contextmanager
@@ -122,115 +113,120 @@ class PrettyFormatter(object):
             self._accumulator.append(' ' * self._indent_level)
         self._accumulator.append(string)
 
-    def visit_node(self, node):
+    def _do_format(self, thing):
+        if isinstance(thing, Node):
+            self._format_node(thing)
+        elif isinstance(thing, list):
+            self._format_list(thing)
+        else:
+            self._format_scalar(thing)
+
+    def _format_node(self, node):
         self._print(node.type + ' {\n')
         with self._indent(2):
-            for key, value in node.subnodes.iteritems():
+            for key, value in node.subnodes():
                 self._print(key + ': ')
-                value.accept(self)
+                self._do_format(value)
                 self._print(',\n')
         self._print('}')
 
-    def visit_array(self, elements):
+    def _format_list(self, elements):
         if not elements:
             self._print('[]')
         elif len(elements) == 1:
             self._print('[')
-            elements[0].accept(self)
+            self._do_format(elements[0])
             self._print(']')
         else:
             self._print('[\n')
             with self._indent(2):
                 for element in elements:
-                    element.accept(self)
+                    self._do_format(element)
                     self._print(',\n')
             self._print(']')
 
-    def visit_scalar(self, value):
+    def _format_scalar(self, value):
         self._print(repr(value))
 
 class Translator(object):
-    def translate(self, thing):
-        return thing.accept(self)
-
     def _parse_arguments(self, arg_nodes):
         arguments = []
-        for arg_node in arg_nodes.elements:
-            assert not arg_node.subnodes['byRef'].value
-            value = self.translate(arg_node.subnodes['value'])
+        for arg_node in arg_nodes:
+            assert not arg_node['byRef']
+            value = self.translate(arg_node['value'])
             arguments.append(value)
         return arguments
 
     def _parse_call(self, node, name_tag):
-        callable_name = self._parse_name(node.subnodes[name_tag])
+        callable_name = self._parse_name(node[name_tag])
         return ast.Call(
             func=ast.Name(id=callable_name, ctx=ast.Load),
-            args=self._parse_arguments(node.subnodes['args']),
+            args=self._parse_arguments(node['args']),
             keywords=[],
             starargs=None,
             kwargs=None,
         )
 
     def _parse_name(self, name_node):
-        return '.'.join(scalar.value for scalar in name_node.subnodes['parts'].elements)
+        return '.'.join(scalar for scalar in name_node['parts'])
 
-    def visit_node(self, node):
+    def translate(self, node):
         if node.type == 'Expr_Include':
-            include_string = node.subnodes['expr'].subnodes['value'].value
+            include_string = node['expr']['value']
             return ast.Import(names=[ast.alias(name=include_string, asname=None)])
         elif node.type == 'Expr_Assign':
-            variable_name = node.subnodes['var'].subnodes['name'].value
-            value = self.translate(node.subnodes['expr'])
+            variable_name = node['var']['name']
+            value = self.translate(node['expr'])
             return ast.Assign(targets=[ast.Name(id=variable_name, ctx=ast.Store)], value=value)
         elif node.type == 'Expr_New':
             return self._parse_call(node, 'class')
         elif node.type == 'Expr_FuncCall':
             return self._parse_call(node, 'name')
         elif node.type.startswith('Scalar_'):
-            return node.subnodes['value'].accept(self)
+            return self.translate_scalar(node['value'])
         elif node.type == 'Stmt_TryCatch':
-            body = [self.translate(child) for child in node.subnodes['stmts'].elements]
+            body = [self.translate(child) for child in node['stmts']]
             except_handlers = []
-            for catch_node in node.subnodes['catches'].elements:
+            for catch_node in node['catches']:
                 except_handlers.append(
                     ast.ExceptHandler(
-                        type=ast.Str(self._parse_name(catch_node.subnodes['type'])),
-                        name=ast.Str(catch_node.subnodes['var'].value),
+                        type=ast.Str(self._parse_name(catch_node['type'])),
+                        name=ast.Str(catch_node['var']),
                         body=[
                             self.translate(child)
-                            for child in catch_node.subnodes['stmts'].elements
+                            for child in catch_node['stmts']
                         ],
                     ),
                 )
             return ast.TryExcept(body=body, handlers=except_handlers, orelse=[])
         elif node.type == 'Expr_MethodCall':
-            target = node.subnodes['var'].subnodes['name'].value
-            name = node.subnodes['name'].value
+            target = node['var']['name']
+            name = node['name']
             return ast.Call(
                 func=ast.Attribute(
                     value=ast.Name(id=target, ctx=ast.Load),
                     attr=name,
                     ctx=ast.Load,
                 ),
-                args=self._parse_arguments(node.subnodes['args']),
+                args=self._parse_arguments(node['args']),
                 keywords=[],
                 starargs=None,
                 kwargs=None,
             )
         elif node.type == 'Expr_Concat':
             return ast.BinOp(
-                left=self.translate(node.subnodes['left']),
+                left=self.translate(node['left']),
                 op=ast.Add(),
-                right=self.translate(node.subnodes['right']),
+                right=self.translate(node['right']),
             )
         elif node.type == 'Expr_Exit':
             return ast.Expr(value=ast.Str('exit!'))
         elif node.type == 'Expr_Variable':
-            return ast.Name(id=node.subnodes['name'].value, ctx=ast.Load)
+            return ast.Name(id=node['name'], ctx=ast.Load)
         elif node.type == 'Stmt_Echo':
             return ast.Print(
                 dest=None,
-                values=[self.translate(child) for child in node.subnodes['exprs'].elements],
+                values=[self.translate(child) for child in node['exprs']],
                 nl=True,
             )
         else:
@@ -238,10 +234,7 @@ class Translator(object):
             print "don't know how to handle %r" % node.type
             return ast.Expr(value=ast.Str('unknown %s' % node.type))
 
-    def visit_array(self, elements):
-        return ast.List(elts=[self.translate(element) for element in elements], ctx=ast.Load)
-
-    def visit_scalar(self, value):
+    def translate_scalar(self, value):
         if isinstance(value, basestring):
             return ast.Str(value)
         elif isinstance(value, (bool, types.NoneType)):
@@ -252,15 +245,16 @@ class Translator(object):
             return ast.Str('unknown scalar %r' % value)
 
 def main():
+    parser = XmlPhpParseTreeReader()
     with open(sys.argv[1]) as stream:
-        statements = parse_php(stream)
+        statements = parser.parse_php(stream)
 
     formatter = PrettyFormatter()
     print formatter.pretty_format(statements)
     print '----'
 
     translator = Translator()
-    statements = [translator.translate(statement) for statement in statements.elements]
+    statements = [translator.translate(statement) for statement in statements]
     module = ast.Module(body=statements)
     print ast.dump(module)
     print '----'
